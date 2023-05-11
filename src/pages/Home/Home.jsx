a654de78baa2b6f62c5f80a7e00e3a092f9eb1cb
Njong <njongemy@gmail.com>
2023-05-11 09:20:50 +0100

    Auto stash before merge of "test" and "origin/test"

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,26 @@ import DiscoverPage from "../../components/DiscoverPage";
 //import Navbar from "../../layouts/NavLayout";
 
 export default function Home(){
+    //    useEffect(() => {
+//     const fetchUser = async () => {
+//         const response = await fetch('http://localhost:4000/api/user/' + user.id, {
+//             headers:{
+//                 'Authorization': `Bearer ${user.token}`
+//             }
+//         })
+//         const json =  await response.json()
+
+//         if(response.ok){
+//             dispatch({type: 'SET_USER_DETAILS', payload:json})
+//         }
+//     }
+              
+//     if(user){
+//         fetchUser()
+//         console.log(userDetails)
+//     }
+        
+//    }, []);
     return(
        <>
       
